58b84f1e2a5319527c1fca53c6c00d5b3f9750eb
Anthony Oneshan <anthonyoneshan@gmail.com>
2025-07-04 23:41:07 -0400

    Initial submission

--- public/assets/Js/main.js
+++ public/assets/Js/main.js
@@ -91,3 +91,15 @@ sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
 sr.reveal(`.about__data, .join__image`, {origin: 'right'})
 sr.reveal(`.about__image, .join__data`, {origin: 'left'})
 sr.reveal(`.popular__card`, {interval: 200})
+
+
+
+/*=============== Masonry ===============*/
+document.addEventListener("DOMContentLoaded", function () {
+  new Masonry("#masonry-grid", {
+    itemSelector: "a",
+    columnWidth: ".masonry-img",
+    percentPosition: true,
+    gutter: 10,
+  });
+});
